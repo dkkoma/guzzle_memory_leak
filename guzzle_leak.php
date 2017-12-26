cec9ac5f20b1ce499401157e0000d2316e5e3879
dkkoma <daisuke_komazaki+github@voyagegroup.com>
2017-12-26 10:37:45 +0900

    init

---
+++ guzzle_leak.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
+
+
+$client = new GuzzleHttp\Client;
+for ($i = 0; $i < 10; $i++)
+{
+    http($client, 10);
+}
+
+function http($client, $limit)
+{
+    for($i = 1; $i <= $limit; $i++) {
+        $tracesize = 0;
+        try {
+            $config = [
+            ];
+            $response = $client->request('GET', 'https://httpbin.org/status/400', $config);
+        } catch (\Exception $e) {
+            //xdebug_debug_zval('e');
+            //file_put_contents('/tmp/debug', print_r($e, true));
+        } finally {
+            unset ($config, $response, $e);
+        }
+        if ($i % $limit === 0) {
+            $usage = memory_get_usage();
+            //xdebug_memory_usage();
+            echo "$i:$usage B\n";
+        }
+    }
+}
